80b64be2cecce767dc8b87c8635ed16220fee587
Matias <5chitt4k3r@gmail.com>
2025-11-24 12:09:18 +0100

    Modificacion del InfoSection

--- trabajo-react/src/components/InfoSection.jsx
+++ trabajo-react/src/components/InfoSection.jsx
@@ -2,23 +2,23 @@ import React from 'react';
 
 const InfoSection = ({ title, text, imageSrc, reverse }) => {
   return (
-    <section className="">
+    <section className="InfoSection">
       <div className={` ${reverse ? '' : ''}`}>
         
         {/* Contenido Texto */}
-        <div className="">
-          <h2 className="">{title}</h2>
-          <p className="">
+        <div className="Contenido de texto">
+          <h2 className="TituloDelInfoSection">{title}</h2>
+          <p className="TextoInfoSection">
             {text}
           </p>
         </div>
 
         {/* Contenido Imagen */}
-        <div className="">
+        <div className="ContenidoImagenInfoSection">
           <img 
             src={imageSrc} 
             alt={title} 
-            className=""
+            className="Imagen"
           />
         </div>
       </div>
