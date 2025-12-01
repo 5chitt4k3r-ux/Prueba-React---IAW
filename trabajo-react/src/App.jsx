f7e73997f056bfbd67a42debdd91b8bb878a4b6e
MTomasNievasM <marcelotomasnm@gmail.com>
2025-12-01 12:27:24 +0100

    Primer Section Completado

--- trabajo-react/src/App.jsx
+++ trabajo-react/src/App.jsx
@@ -1,11 +1,29 @@
-import Header from "./components/Header/Header";
-import Hero from "./components/Hero/Hero";
+import React from 'react';
+import './App.css';
 
-export default function App() {
+import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
+import Section from './components/Section/Section';
+
+// --- CORRECCIÓN AQUÍ ---
+import cajeraImg from './media/Cajera.jpeg'; 
+// Si tienes la otra imagen también en media:
+// import imagenHeader from './media/ImagenHeader.jpg';
+
+function App() {
   return (
-    <>
+    <div className="App">
       <Header />
       <Hero />
-    </>
+      
+      <Section 
+        title="Atención al cliente" 
+        text="Nuestro equipo está siempre disponible para ayudarte." 
+        imageSrc={cajeraImg}
+        inverted={true}
+      />
+    </div>
   );
 }
+
+export default App;
